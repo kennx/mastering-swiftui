244a9a48a1aac5b2c941a28066dd406752e3a654
zhangjian <zhangxjian@me.com>
2021-07-24 15:03:06 +0800

    update exercise 2

--- SwiftUIScrollView/SwiftUIScrollView/ContentView.swift
+++ SwiftUIScrollView/SwiftUIScrollView/ContentView.swift
@@ -9,24 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    ScrollView {
-      HStack {
-        VStack(alignment: .leading) {
-          Text("monday, aug 20".uppercased())
-            .foregroundColor(.secondary)
-            .font(.caption)
-          Text("Your Reading")
-            .font(.system(.largeTitle, design: .rounded))
-            .fontWeight(.black)
+    ScrollView(.horizontal, showsIndicators: false) {
+      VStack(alignment: .leading) {
+        HStack {
+          VStack(alignment: .leading) {
+            Text("monday, aug 20".uppercased())
+              .font(.caption)
+              .foregroundColor(.secondary)
+            Text("Your Reading")
+              .font(.system(.largeTitle, design: .rounded))
+              .fontWeight(.black)
+          }
+          .layoutPriority(100)
+          Spacer()
         }
-        .padding()
+        .padding([.top, .horizontal])
+        .layoutPriority(-100)
+        
+        HStack {
+          CardView(image: "swiftui-button", category: "SwiftUI", heading: "Drawing a Border with Rounded Corners", author: "Simon Ng")
+            .frame(width: 300)
+          CardView(image: "macos-programming", category: "macOS", heading: "Building a Simple Editing App", author: "Gabriel Theodoropoulos")
+            .frame(width: 300)
+          CardView(image: "flutter-app", category: "Flutter", heading: "Building Complex Layout with Flutter", author: "Lawrence Tan")
+            .frame(width: 300)
+          CardView(image: "natural-language-api", category: "iOS", heading: "What's New in Natural Language API", author: "Sai Kambampati")
+            .frame(width: 300)
+        }
+        
         Spacer()
-      }
-      VStack {
-        CardView(image: "swiftui-button", category: "SwiftUI", heading: "Drawing a Border with Rounded Corners", author: "Simon Ng")
-        CardView(image: "macos-programming", category: "macOS", heading: "Building a Simple Editing App", author: "Gabriel Theodoropoulos")
-        CardView(image: "flutter-app", category: "Flutter", heading: "Building Complex Layout with Flutter", author: "Lawrence Tan")
-        CardView(image: "natural-language-api", category: "iOS", heading: "What's New in Natural Language API", author: "Sai Kambampati")
       }
     }
   }
