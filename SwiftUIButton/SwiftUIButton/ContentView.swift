f647c8ad71b4ba58c84d032d185c5caab6dc8072
zhangjian <zhangxjian@me.com>
2021-07-24 16:00:45 +0800

    update

--- SwiftUIButton/SwiftUIButton/ContentView.swift
+++ SwiftUIButton/SwiftUIButton/ContentView.swift
@@ -9,22 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    Button(action: {
-      print("Hello World tapped!")
-    }) {
-      Text("Hello World")
-        .fontWeight(.bold)
-        .font(.title)
-        .padding()
-        .background(Color.purple)
-        .cornerRadius(40)
-        .foregroundColor(.white)
-        .padding(10)
-        .overlay(
-          RoundedRectangle(cornerRadius: 40)
-            .stroke(Color.purple, lineWidth: 5)
-        )
+    VStack {
+      
+      
+      Button(action: {
+        print("Hello World tapped!")
+      }) {
+        HStack {
+          Image(systemName: "square.and.arrow.up")
+            .font(.title)
+          Text("Share")
+            .fontWeight(.semibold)
+            .font(.title)
+        }
+      }
+      .buttonStyle(GradienBackgroundStyle())
+      
+      Button(action: {
+        print("Hello World tapped!")
+      }) {
+        HStack {
+          Image(systemName: "square.and.pencil")
+            .font(.title)
+          Text("Edit")
+            .fontWeight(.semibold)
+            .font(.title)
+        }
+      }
+      .buttonStyle(GradienBackgroundStyle())
+      
+      
+      Button(action: {
+        print("Hello World tapped!")
+      }) {
+        HStack {
+          Image(systemName: "trash")
+            .font(.title)
+          Text("Delete")
+            .fontWeight(.semibold)
+            .font(.title)
+        }
+      }
+      .buttonStyle(GradienBackgroundStyle())
+      
+      
     }
+  }
+}
+
+
+struct GradienBackgroundStyle: ButtonStyle {
+  func makeBody(configuration: Configuration) -> some View {
+    configuration.label
+      .frame(minWidth: 0, maxWidth: .infinity)
+      .padding()
+      .foregroundColor(.white)
+      .background(LinearGradient(gradient: Gradient(colors: [Color("Dark Green"), Color("Light Green")]), startPoint: .leading, endPoint: .trailing))
+      .cornerRadius(40)
+      .padding(.horizontal, 20)
+      .scaleEffect(configuration.isPressed ? 0.9 : 1.0)
   }
 }
 
