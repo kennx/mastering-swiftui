f1f13c2c9892229ccf6276102df18bddd0fb2926
zhangjian <zhangxjian@me.com>
2021-07-24 14:32:11 +0800

    update stacks exercise 1

--- SwiftUIStacks/SwiftUIStacks/ContentView.swift
+++ SwiftUIStacks/SwiftUIStacks/ContentView.swift
@@ -12,36 +12,14 @@ struct ContentView: View {
     VStack {
       HeaderView()
       
-      HStack(spacing: 15) {
-        PricingView(title: "Basic", price: "$9", textColor: .white, bgColor: .purple)
-        
-        ZStack {
-          PricingView(title: "Pro", price: "$19", textColor: .black, bgColor: Color(red: 240/255, green: 240/255, blue: 240/255))
-          
-          Text("Best for designer")
-            .font(.system(.caption, design: .rounded))
-            .fontWeight(.bold)
-            .foregroundColor(.white)
-            .padding(5)
-            .background(Color(red: 255/255, green: 183/255, blue: 37/255))
-            .offset(x: 0, y: 87)
+      ZStack(alignment: .top) {
+          PricingView(title: "Basic", price: "$9", textColor: .white, bgColor: .purple, icon: "seal.fill")
+        PricingView(title: "Pro", price: "$19", textColor: .white, bgColor: .yellow, icon: "dial.min")
+            .offset(y: 120)
+          PricingView(title: "Team", price: "$299", textColor: .white, bgColor: Color(red: 62/255, green: 63/255, blue: 70/255), icon: "wand.and.rays")
+            .offset(y: 240)
         }
-      }
-      .padding(.horizontal)
-      
-      ZStack {
-        PricingView(title: "Team", price: "$299", textColor: .white, bgColor: Color(red: 62/255, green: 63/255, blue: 70/255), icon: "wand.and.rays")
-          .padding()
-        
-        Text("Perfect for teams with 20 members")
-          .font(.system(.caption, design: .rounded))
-          .fontWeight(.bold)
-          .foregroundColor(.white)
-          .padding(5)
-          .background(Color(red: 255/255, green: 183/255, blue: 37/255))
-          .offset(x: 0, y: 87)
-      }
-      
+        .padding(.horizontal)
       Spacer()
     }
   }
@@ -99,7 +77,7 @@ struct PricingView: View {
         .font(.headline)
         .foregroundColor(textColor)
     }
-    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
+    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 300, alignment: .top)
     .padding(40)
     .background(bgColor)
     .cornerRadius(10)
