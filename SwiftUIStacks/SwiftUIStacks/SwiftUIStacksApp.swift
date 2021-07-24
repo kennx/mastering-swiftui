118a498e282dfbac3304142c15bd45bac0ca6ec7
zhangjian <zhangxjian@me.com>
2021-07-24 14:13:19 +0800

    update

---
+++ SwiftUIStacks/SwiftUIStacks/SwiftUIStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStacksApp.swift
+//  SwiftUIStacks
+//
+//  Created by 张剑 on 2021/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
