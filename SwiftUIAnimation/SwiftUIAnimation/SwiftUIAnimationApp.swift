b6e0d51d56e47095263f5100bd1da5456dc0c609
zhangjian <zhangxjian@me.com>
2021-07-26 14:02:42 +0800

    “第 9 章 基礎動畫與轉場”

---
+++ SwiftUIAnimation/SwiftUIAnimation/SwiftUIAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationApp.swift
+//  SwiftUIAnimation
+//
+//  Created by 张剑 on 2021/7/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
