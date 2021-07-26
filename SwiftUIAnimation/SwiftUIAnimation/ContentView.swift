ced1e4d7d063ccc7dc033bfac237849068aec8b0
zhangjian <zhangxjian@me.com>
2021-07-26 15:48:00 +0800

    原點下載指示器

--- SwiftUIAnimation/SwiftUIAnimation/ContentView.swift
+++ SwiftUIAnimation/SwiftUIAnimation/ContentView.swift
@@ -8,32 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var progress: CGFloat = 0.0
+  @State private var isLoading: Bool = false
   
   var body: some View {
-    ZStack {
-      Text("\(Int(progress * 100))%")
-        .font(.system(.title, design: .rounded))
-        .bold()
-      
-      Circle()
-        .stroke(Color(.systemGray5), lineWidth: 10)
-        .frame(width: 150, height: 150)
-      
-      Circle()
-        .trim(from: 0, to: progress)
-        .stroke(Color.green, lineWidth: 10)
-        .frame(width: 150, height: 150)
-        .rotationEffect(Angle(degrees: -90))
+    HStack {
+      ForEach(0...4, id: \.self) { index in
+        Circle()
+          .frame(width: 10, height: 10)
+          .foregroundColor(.green)
+          .scaleEffect(self.isLoading ? 0 : 1)
+          .animation(Animation.linear(duration: 0.6).repeatForever().delay(0.2 * Double(index)))
+      }
     }
     .onAppear() {
-      Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { timer in
-        self.progress += 0.05
-        print(self.progress)
-        if self.progress >= 1.0 {
-          timer.invalidate()
-        }
-      }
+      self.isLoading = true
     }
   }
 }
