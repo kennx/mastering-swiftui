ec6d2d6afe2b4e6fee6fb2f2c94d0256979e7410
zhangjian <zhangxjian@me.com>
2021-07-26 11:12:36 +0800

    update 画饼😂

--- SwiftUIShape/SwiftUIShape/ContentView.swift
+++ SwiftUIShape/SwiftUIShape/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  var body: some View {
+    ZStack {
+      Path() { path in
+        path.move(to: CGPoint(x: 200, y: 200))
+        path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 0.0), endAngle: Angle(degrees: 190), clockwise: true)
+      }
+      .fill(Color(.systemYellow))
+      
+      Path() { path in
+        path.move(to: CGPoint(x: 200, y: 200))
+        path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 190), endAngle: Angle(degrees: 110), clockwise: true)
+      }
+      .fill(Color(.systemTeal))
+      
+      Path() { path in
+        path.move(to: CGPoint(x: 200, y: 200))
+        path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 110), endAngle: Angle(degrees: 90), clockwise: true)
+      }
+      .fill(Color(.systemBlue))
+      
+      Path() { path in
+        path.move(to: CGPoint(x: 200, y: 200))
+        path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 90), endAngle: Angle(degrees: 360), clockwise: true)
+      }
+      .fill(Color(.systemPurple))
+      .offset(x: 20, y: 20)
+      
+      Path() { path in
+        path.move(to: CGPoint(x: 200, y: 200))
+        path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 90), endAngle: Angle(degrees: 360), clockwise: true)
+        path.closeSubpath()
+      }
+      .stroke(Color(red: 52/255, green: 52/255, blue: 122/255), lineWidth: 10)
+      .offset(x: 20, y: 20)
+      .overlay(
+        Text("25%")
+          .font(.system(.largeTitle, design: .rounded))
+          .bold()
+          .foregroundColor(.white)
+          .offset(x: 65, y: -135)
+      )
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
