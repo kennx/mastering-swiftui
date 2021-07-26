408813a94c07c81d227876cdb87c3736526ac6ae
zhangjian <zhangxjian@me.com>
2021-07-26 11:48:24 +0800

    “以 Dome 形狀建立一個按鈕”

--- SwiftUIShape/SwiftUIShape/ContentView.swift
+++ SwiftUIShape/SwiftUIShape/ContentView.swift
@@ -9,47 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    ZStack {
-      Path() { path in
-        path.move(to: CGPoint(x: 200, y: 200))
-        path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 0.0), endAngle: Angle(degrees: 190), clockwise: true)
-      }
-      .fill(Color(.systemYellow))
+    Button(action: {
       
-      Path() { path in
-        path.move(to: CGPoint(x: 200, y: 200))
-        path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 190), endAngle: Angle(degrees: 110), clockwise: true)
-      }
-      .fill(Color(.systemTeal))
-      
-      Path() { path in
-        path.move(to: CGPoint(x: 200, y: 200))
-        path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 110), endAngle: Angle(degrees: 90), clockwise: true)
-      }
-      .fill(Color(.systemBlue))
-      
-      Path() { path in
-        path.move(to: CGPoint(x: 200, y: 200))
-        path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 90), endAngle: Angle(degrees: 360), clockwise: true)
-      }
-      .fill(Color(.systemPurple))
-      .offset(x: 20, y: 20)
-      
-      Path() { path in
-        path.move(to: CGPoint(x: 200, y: 200))
-        path.addArc(center: .init(x: 200, y: 200), radius: 150, startAngle: Angle(degrees: 90), endAngle: Angle(degrees: 360), clockwise: true)
-        path.closeSubpath()
-      }
-      .stroke(Color(red: 52/255, green: 52/255, blue: 122/255), lineWidth: 10)
-      .offset(x: 20, y: 20)
-      .overlay(
-        Text("25%")
-          .font(.system(.largeTitle, design: .rounded))
-          .bold()
-          .foregroundColor(.white)
-          .offset(x: 65, y: -135)
-      )
+    }) {
+      Text("Test")
+        .font(.system(.title, design: .rounded))
+        .bold()
+        .foregroundColor(.white)
+        .frame(width: 250, height: 50)
+        .background(Dome().fill(Color.red))
     }
+  }
+}
+
+struct Dome: Shape {
+  func path(in rect: CGRect) -> Path {
+    var path = Path()
+    path.move(to: CGPoint(x: 0, y: 0))
+    path.addQuadCurve(to: CGPoint(x: rect.size.width, y: 0), control: CGPoint(x: rect.size.width/2, y: -(rect.size.width*0.1)))
+    path.addRect(CGRect(x: 0, y: 0, width: rect.size.width, height: rect.size.height))
+    
+    return path
   }
 }
 
