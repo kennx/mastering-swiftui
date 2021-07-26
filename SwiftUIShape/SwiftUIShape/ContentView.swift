a2cde97c47392f6704e6cf2c1d5a2f826e6a1fca
zhangjian <zhangxjian@me.com>
2021-07-26 13:55:59 +0800

    “繪製進度視圖”

--- SwiftUIShape/SwiftUIShape/ContentView.swift
+++ SwiftUIShape/SwiftUIShape/ContentView.swift
@@ -8,30 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+  private var purpleGradient = LinearGradient(gradient: Gradient(colors: [Color(red: 207/255, green: 150/255, blue: 207/255), Color(red: 107/255, green: 116/255, blue: 179/255)]), startPoint: .leading, endPoint: .trailing)
   var body: some View {
-    Button(action: {
+    ZStack {
+      Circle()
+        .stroke(Color(.systemGray6), lineWidth: 20)
+        .frame(width: 300, height: 300)
       
-    }) {
-      Text("Test")
-        .font(.system(.title, design: .rounded))
-        .bold()
-        .foregroundColor(.white)
-        .frame(width: 250, height: 50)
-        .background(Dome().fill(Color.red))
+      Circle()
+        .trim(from: 0.0, to: 0.85)
+        .stroke(purpleGradient, lineWidth: 20)
+        .frame(width: 300, height: 300)
+        .overlay(
+          VStack {
+            Text("85%")
+              .font(.system(size: 80, weight: .bold, design: .rounded))
+              .foregroundColor(Color(.systemGray))
+            Text("Complete")
+              .font(.system(.body, design: .rounded))
+              .bold()
+              .foregroundColor(Color(.systemGray))
+          }
+        )
     }
   }
 }
 
-struct Dome: Shape {
-  func path(in rect: CGRect) -> Path {
-    var path = Path()
-    path.move(to: CGPoint(x: 0, y: 0))
-    path.addQuadCurve(to: CGPoint(x: rect.size.width, y: 0), control: CGPoint(x: rect.size.width/2, y: -(rect.size.width*0.1)))
-    path.addRect(CGRect(x: 0, y: 0, width: rect.size.width, height: rect.size.height))
-    
-    return path
-  }
-}
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
