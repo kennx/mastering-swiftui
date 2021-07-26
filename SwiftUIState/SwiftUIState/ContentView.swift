fc38a79f4dd8204192c480179d0510eb2d4a3310
zhangjian <zhangxjian@me.com>
2021-07-26 09:38:19 +0800

    update 2

--- SwiftUIState/SwiftUIState/ContentView.swift
+++ SwiftUIState/SwiftUIState/ContentView.swift
@@ -12,12 +12,28 @@ struct ContentView: View {
   @State private var counter = 1
   
   var body: some View {
+    VStack {
+      CounterButton(counter: $counter, color: .blue)
+      CounterButton(counter: $counter, color: .green)
+      CounterButton(counter: $counter, color: .red)
+    }
+  }
+}
+
+
+struct CounterButton: View {
+  
+  @Binding var counter: Int
+  
+  var color: Color
+  
+  var body: some View {
     Button(action: {
       self.counter += 1
     }) {
       Circle()
         .frame(width: 200, height: 200)
-        .foregroundColor(.red)
+        .foregroundColor(color)
         .overlay(
           Text("\(counter)")
             .font(.system(size: 100, weight: .bold, design: .rounded))
@@ -26,6 +42,7 @@ struct ContentView: View {
     }
   }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
