b54af8694c875faa577be0faad577586ef57fee3
zhangjian <zhangxjian@me.com>
2021-07-26 14:00:55 +0800

    畫出甜甜圈圖

--- SwiftUIShape/SwiftUIShape/ContentView.swift
+++ SwiftUIShape/SwiftUIShape/ContentView.swift
@@ -8,29 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-  private var purpleGradient = LinearGradient(gradient: Gradient(colors: [Color(red: 207/255, green: 150/255, blue: 207/255), Color(red: 107/255, green: 116/255, blue: 179/255)]), startPoint: .leading, endPoint: .trailing)
   var body: some View {
     ZStack {
       Circle()
-        .stroke(Color(.systemGray6), lineWidth: 20)
-        .frame(width: 300, height: 300)
+        .trim(from: 0.0, to: 0.4)
+        .stroke(Color(.systemBlue), lineWidth: 80)
       
       Circle()
-        .trim(from: 0.0, to: 0.85)
-        .stroke(purpleGradient, lineWidth: 20)
-        .frame(width: 300, height: 300)
+        .trim(from: 0.4, to: 0.6)
+        .stroke(Color(.systemTeal), lineWidth: 80)
+      
+      Circle()
+        .trim(from: 0.6, to: 0.75)
+        .stroke(Color(.systemPurple), lineWidth: 80)
+      
+      Circle()
+        .trim(from: 0.75, to: 1.0)
+        .stroke(Color(.systemYellow), lineWidth: 90)
         .overlay(
-          VStack {
-            Text("85%")
-              .font(.system(size: 80, weight: .bold, design: .rounded))
-              .foregroundColor(Color(.systemGray))
-            Text("Complete")
-              .font(.system(.body, design: .rounded))
-              .bold()
-              .foregroundColor(Color(.systemGray))
-          }
+          Text("25%")
+            .font(.system(.title, design: .rounded))
+            .bold()
+            .foregroundColor(.white)
+            .offset(x: 120, y: -100)
         )
     }
+    .frame(width: 300, height: 300)
   }
 }
 
