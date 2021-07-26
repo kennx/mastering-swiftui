5c73a8195a9d8fc8d0cd24df032300884c6f30fa
zhangjian <zhangxjian@me.com>
2021-07-26 14:50:30 +0800

    下載指示器

--- SwiftUIAnimation/SwiftUIAnimation/ContentView.swift
+++ SwiftUIAnimation/SwiftUIAnimation/ContentView.swift
@@ -8,28 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var circleColorChanged = false
-  @State private var heartColorChanged = false
-  @State private var heartSizeChanged = false
+  @State private var isLoading = false
   
   var body: some View {
     ZStack {
-      Circle()
-        .frame(width: 200, height: 200)
-        .foregroundColor(circleColorChanged ? Color(.systemGray5) : .red)
-        .animation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3))
       
-      Image(systemName: "heart.fill")
-        .foregroundColor(heartColorChanged ? .red : .white)
-        .font(.system(size: 100))
-        .animation(nil) // 從此處取消動畫
-        .scaleEffect(heartSizeChanged ? 1.0 : 0.5)
-        .animation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3))
-    }
-    .onTapGesture {
-      self.circleColorChanged.toggle()
-      self.heartColorChanged.toggle()
-      self.heartSizeChanged.toggle()
+      Circle()
+        .stroke(Color(.systemGray5), lineWidth: 14)
+        .frame(width: 100, height: 100)
+      
+      Circle()
+        .trim(from: 0, to: 0.2)
+        .stroke(Color.green, lineWidth: 7)
+        .frame(width: 100, height: 100)
+        .rotationEffect(Angle(degrees: isLoading ? 360 : 0))
+        .animation(Animation.linear(duration: 0.5).repeatForever(autoreverses: false))
+        .onAppear() {
+          self.isLoading = true
+      }
     }
   }
 }
