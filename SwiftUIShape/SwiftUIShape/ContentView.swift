a33a1ff62180f00e4fb4155dda64785bbdb2d515
zhangjian <zhangxjian@me.com>
2021-07-26 10:24:46 +0800

    第 8 章 實作路徑與形狀來畫線與圓餅圖”

---
+++ SwiftUIShape/SwiftUIShape/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIShape
+//
+//  Created by 张剑 on 2021/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIShape/SwiftUIShape/SwiftUIShapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIShapeApp.swift
+//  SwiftUIShape
+//
+//  Created by 张剑 on 2021/7/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIShapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
