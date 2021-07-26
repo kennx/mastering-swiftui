012ea162a81f397a9d32101e1e647c692477810d
zhangjian <zhangxjian@me.com>
2021-07-26 09:07:33 +0800

    update

--- SwiftUIState/SwiftUIState/ContentView.swift
+++ SwiftUIState/SwiftUIState/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  
+  @State private var isPlaying = false
+  
+  var body: some View {
+    Button(action: {
+      self.isPlaying.toggle()
+    }) {
+      Image(systemName: isPlaying ? "stop.circle.fill" : "play.circle.fill")
+        .font(.system(size: 150))
+        .foregroundColor(isPlaying ? .red : .green)
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
