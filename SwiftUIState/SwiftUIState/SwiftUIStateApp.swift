8e09cdd7dc7a0179e06829854276f1982e9ac617
zhangjian <zhangxjian@me.com>
2021-07-26 09:02:18 +0800

    第 7 章 狀態與綁定

---
+++ SwiftUIState/SwiftUIState/SwiftUIStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStateApp.swift
+//  SwiftUIState
+//
+//  Created by 张剑 on 2021/7/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
