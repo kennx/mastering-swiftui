b8073d24747137d216d309d85d23d52de34a7b65
zhangjian <zhangxjian@me.com>
2021-07-27 09:33:12 +0800

    “使用動畫與轉場來建立酷炫按鈕”

--- SwiftUIAnimation/SwiftUIAnimation/ContentView.swift
+++ SwiftUIAnimation/SwiftUIAnimation/ContentView.swift
@@ -9,50 +9,85 @@ import SwiftUI
 
 struct ContentView: View {
   
-  @State private var show = false
+  @State private var isSubmit = false
+  @State private var rotationCircle = false
+  @State private var isDone = false
   
   var body: some View {
     VStack {
-      RoundedRectangle(cornerRadius: 10)
-        .frame(width: 300, height: 300)
-        .foregroundColor(.green)
-        .overlay(
-          Text("Show details")
-            .font(.system(.largeTitle, design: .rounded))
+      ZStack {
+        RoundedRectangle(cornerRadius: 28)
+          .foregroundColor(isDone ? .red : .green)
+          .frame(width: isSubmit ? 300 : 200, height: 56)
+          .animation(.easeInOut(duration: 0.1))
+        
+        if !isSubmit {
+          Text("Submit")
+            .font(.system(.title, design: .rounded))
             .bold()
             .foregroundColor(.white)
-        )
-      
-      if show {
-        RoundedRectangle(cornerRadius: 10)
-          .frame(width: 300, height: 300)
-          .foregroundColor(.purple)
-          .overlay(
-            Text("Well, here is the details")
-              .font(.system(.largeTitle, design: .rounded))
+        }
+        
+        if isSubmit && !isDone {
+          HStack {
+            Circle()
+              .trim(from: 0.0, to: 0.8)
+              .stroke(Color.white, lineWidth: 4)
+              .frame(width: 26, height: 26)
+              .rotationEffect(Angle(degrees: self.rotationCircle ? 360 : 0))
+              .animation(.linear.repeatForever(autoreverses: false))
+            
+            Text("Progressing")
+              .font(.system(.title, design: .rounded))
               .bold()
               .foregroundColor(.white)
-        )
-          .transition(.scaleAndOffset)
+          }
+          .onAppear() {
+            self.rotationCircle.toggle()
+            self.startProcessing()
+          }
+        }
+        
+        if isDone {
+          Text("Done")
+            .font(.system(.title, design: .rounded))
+            .bold()
+            .foregroundColor(.white)
+            .onAppear() {
+              self.doneProcessing()
+            }
+        }
+        
+        
       }
     }
     .onTapGesture {
-      withAnimation(.spring()) {
-        self.show.toggle()
+      if !self.rotationCircle && !self.isDone {
+        self.isSubmit.toggle()
       }
     }
   }
+  
+  private func startProcessing() {
+    // 使用DispatchQueue.main.asyncAfter 模擬一個處理程序
+    // 在實際的專案中，你需要在這邊執行一項任務
+    // 任務完成之後，將完成狀態設定為 true
+    DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+      self.isDone.toggle()
+      self.rotationCircle.toggle()
+    }
+  }
+  
+  private func doneProcessing() {
+    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+      self.isSubmit = false
+      self.rotationCircle = false
+      self.isDone = false
+    }
+  }
+  
 }
 
-
-extension AnyTransition {
-  static var offsetScaleOpacity: AnyTransition {
-    AnyTransition.offset(x: -600, y: 0).combined(with: .scale).combined(with: .opacity)
-  }
-  static var scaleAndOffset: AnyTransition {
-    AnyTransition.asymmetric(insertion: .scale(scale: 0, anchor: .bottom), removal: .offset(x: -600, y: 0))
-  }
-}
 
 
 struct ContentView_Previews: PreviewProvider {
