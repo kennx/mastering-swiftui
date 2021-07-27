872e70ad98ad3473050c7ed228b2bd3aae65aeaf
zhangjian <zhangxjian@me.com>
2021-07-27 11:53:14 +0800

    “第 10 章 動態列表、 ForEach 與 Identifiable 的使用方法”

---
+++ SwiftUIList/SwiftUIList/SwiftUIListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListApp.swift
+//  SwiftUIList
+//
+//  Created by 张剑 on 2021/7/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
