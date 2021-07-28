80e014ad5bc8d5d707d5f8ae014c761d13ecce05
zhangjian <zhangxjian@me.com>
2021-07-28 09:08:49 +0800

    “建立一個複雜列佈局的列表視圖”

--- SwiftUIList/SwiftUIList/ContentView.swift
+++ SwiftUIList/SwiftUIList/ContentView.swift
@@ -33,12 +33,52 @@ struct ContentView: View {
                       Restaurant(name: "CASK Pub and Kitchen", image: "caskpubkitchen")
   ]
   
+  var cards = [Card(title: "The Comprehensive Guide to the State Management in iOS", author: "Alexey Naumov", rating: 4, excerpt: "There are many challenges in the software development, but there is one beast that tends to screw things up much more often than the others: the problem of app’s state management and data propagation.", image: "state-management"),
+               Card(title: "Using Swift Protocols to Manage App Configuration", author: "Gabriel Theodoropoulos", rating: 4, excerpt: "Hello and welcome to a new tutorial! One of the most common concepts met and used in Swift by all developers is protocols, and I don’t think there’s even one developer who doesn’t know about them.", image: "protocols"),
+               Card(title: "Drawing a Border with Rounded Corners", author: "Simon Ng", rating: 4, excerpt: "With SwiftUI, you can easily draw a border around a button or text (and it actually works for all views) using the border modifier.", image: "swiftui-button"),
+               Card(title: "Building a Simple Text Editing App", author: "Gabriel Theodoropoulos", rating: 5, excerpt: "Today we are going to focus on a commonly used family of controls which are vital to every application. Their primary purpose is to gather user input as well as to display certain message types to users. We are going to talk about text controls.", image: "macos-programming"),
+               Card(title: "Building a Flutter App with Complex UI", author: "Lawrence Tan", rating: 4, excerpt: "Hello there! Welcome to the second tutorial of our Flutter series. In the last tutorial, you learned the basics of building a Flutter app. So if you have not gone through it, please take a pit stop here, visit it first before proceeding with this tutorial.", image: "flutter-app")
+]
+  
   var body: some View {
-    List(restaurants.indices) { index in
-      if (0...1).contains(index) {
-        FullImageRow(restaurant: restaurants[index])
-      } else {
-        BasicImageRow(restaurant: restaurants[index])
+    List(cards) { card in
+      CardImageRow(card: card)
+    }
+  }
+}
+
+
+struct CardImageRow: View {
+  var card: Card
+  var body: some View {
+    VStack {
+      Image(card.image)
+        .resizable()
+        .scaledToFit()
+      HStack {
+        Text(card.title)
+          .font(.title)
+          .fontWeight(.black)
+        Spacer()
+      }
+      HStack {
+        Text(card.author)
+          .font(.caption)
+          .foregroundColor(.secondary)
+        Spacer()
+      }
+      HStack {
+        ForEach(0 ..< card.rating) { item in
+          Image(systemName: "star.fill")
+            .foregroundColor(.yellow)
+        }
+        Spacer()
+      }
+      HStack {
+        Text(card.excerpt)
+          .font(.footnote)
+          .foregroundColor(.secondary)
+        Spacer()
       }
     }
   }
@@ -75,6 +115,17 @@ struct Restaurant: Identifiable {
   var name: String
   var image: String
 }
+
+
+struct Card: Identifiable {
+  var id: UUID = UUID()
+  var title: String
+  var author: String
+  var rating: Int
+  var excerpt: String
+  var image: String
+}
+
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
