06f5db66518807d4f4bdacb3534f2f5d022bd0f9
zhangjian <zhangxjian@me.com>
2021-07-30 10:26:42 +0800

    複合手勢

--- SwiftUIGestures/SwiftUIGestures/ContentView.swift
+++ SwiftUIGestures/SwiftUIGestures/ContentView.swift
@@ -9,23 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
   
+  @GestureState private var isPressed = false
+  
   @GestureState private var dragOffset = CGSize.zero
   @State private var position = CGSize.zero
   
   var body: some View {
     Image(systemName: "star.circle.fill")
       .font(.system(size: 100))
+      .opacity(isPressed ? 0.5 : 1.0)
       .offset(x: position.width + dragOffset.width, y: position.height + dragOffset.height)
       .animation(.easeInOut)
       .foregroundColor(.green)
       .gesture(
-        DragGesture()
-          .updating($dragOffset) { (value, state, transaction) in
-            state = value.translation
+        
+        LongPressGesture(minimumDuration: 1.0)
+          .updating($isPressed) { (currentState, state, transaction) in
+            state = currentState
           }
-          .onEnded { value in
-            self.position.height += value.translation.height
-            self.position.width += value.translation.width
+          .sequenced(before: DragGesture())
+          .updating($dragOffset) { (value, state, transaction) in
+            switch value {
+            case .first(true):
+              print("Tapping")
+            case .second(true, let drag):
+              state = drag?.translation ?? .zero
+            default:
+              break
+            }
+          }
+          .onEnded { (value) in
+            guard case .second(true, let drag?) = value else {
+              return
+            }
+            self.position.height += drag.translation.height
+            self.position.width += drag.translation.width
           }
       )
   }
