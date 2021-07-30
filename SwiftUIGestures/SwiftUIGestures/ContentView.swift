ce0f00a7e64c21c65bd21e86fd311aa8b5db22e8
zhangjian <zhangxjian@me.com>
2021-07-30 15:48:16 +0800

    MagnificationGesture

--- SwiftUIGestures/SwiftUIGestures/ContentView.swift
+++ SwiftUIGestures/SwiftUIGestures/ContentView.swift
@@ -8,44 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-  
-  @GestureState private var dragState = DragState.inactive
-  
-  @GestureState private var isPressed = false
-  
-  @GestureState private var dragOffset = CGSize.zero
-  @State private var position = CGSize.zero
+
+  @GestureState private var scaleSize: CGFloat = 1.0
   
   var body: some View {
     Image(systemName: "star.circle.fill")
       .font(.system(size: 100))
-      .opacity(dragState.isPressing ? 0.5 : 1.0)
-      .offset(x: position.width + dragOffset.width, y: position.height + dragOffset.height)
-      .animation(.easeInOut)
-      .foregroundColor(.green)
+      .foregroundColor(.purple)
+      .scaleEffect(scaleSize)
+      .animation(.linear)
       .gesture(
-        
-        LongPressGesture(minimumDuration: 1.0)
-          .updating($isPressed) { (currentState, state, transaction) in
-            state = currentState
-          }
-          .sequenced(before: DragGesture())
-          .updating($dragState) { (value, state, transaction) in
-            switch value {
-            case .first(true):
-              state = .pressing
-            case .second(true, let drag):
-              state = .dragging(translation: drag?.translation ?? .zero)
-            default:
-              break
-            }
-          }
-          .onEnded { (value) in
-            guard case .second(true, let drag?) = value else {
-              return
-            }
-            self.position.height += drag.translation.height
-            self.position.width += drag.translation.width
+        MagnificationGesture()
+          .updating($scaleSize) { (value, state, transaction) in
+            state = value
           }
       )
   }
