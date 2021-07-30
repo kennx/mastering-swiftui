5b07ace366030e490554160c96f3274233df0346
zhangjian <zhangxjian@me.com>
2021-07-30 09:46:34 +0800

    GestureState

--- SwiftUIGestures/SwiftUIGestures/ContentView.swift
+++ SwiftUIGestures/SwiftUIGestures/ContentView.swift
@@ -10,16 +10,21 @@ import SwiftUI
 struct ContentView: View {
   
   @State private var isPressed = false
+  @GestureState private var longPressTap = false
   
   var body: some View {
     Image(systemName: "star.circle.fill")
       .font(.system(size: 200))
+      .opacity(longPressTap ? 0.4 : 1.0)
       .scaleEffect(isPressed ? 0.5 : 1.0)
       .animation(.easeInOut)
       .foregroundColor(.green)
       .gesture(
-        TapGesture()
-          .onEnded {
+        LongPressGesture(minimumDuration: 1.0)
+          .updating($longPressTap) { (currentState, state, transaction) in
+            state = currentState
+          }
+          .onEnded { _ in
             self.isPressed.toggle()
           }
       )
